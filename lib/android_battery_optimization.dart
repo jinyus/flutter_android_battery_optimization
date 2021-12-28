b0e3a606c181cd6da9447f879500992e77ad4001
jinyus <starboymc4@gmail.com>
2021-12-28 11:04:43 -0500

    add comments

--- lib/android_battery_optimization.dart
+++ lib/android_battery_optimization.dart
@@ -7,16 +7,24 @@ class AndroidBatteryOptimization {
       const MethodChannel('android_battery_optimization');
 
   static Future<String> get platformVersion async {
-    final String version = await (_channel.invokeMethod('getPlatformVersion') as FutureOr<String>);
+    final String version =
+        await (_channel.invokeMethod('getPlatformVersion') as FutureOr<String>);
     return version;
   }
 
+  // Return whether the given application package name is on the device's power allowlist.
+  // Being on the power allowlist means that the system will not apply most power saving features to the app.
+  // You should check run this method before opening settings.
+  // ref:  https://developer.android.com/reference/android/os/PowerManager#isIgnoringBatteryOptimizations(java.lang.String)
   static Future<bool> get isIgnoringBatteryOptimizations async {
-    final bool status =
-        await (_channel.invokeMethod('isIgnoringBatteryOptimizations') as FutureOr<bool>);
+    final bool status = await (_channel
+        .invokeMethod('isIgnoringBatteryOptimizations') as FutureOr<bool>);
     return status;
   }
 
+  // Open Battery Optimization Settings Page
+  // showToast determines whether instructions will be given to the user
+  // (in the form of a toast message) on how to disable optimization
   static Future<void> openBatteryOptimizationSetting(
       {bool showToast = false}) async {
     await _channel.invokeMethod(
